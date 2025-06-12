47590b46d5210bcbbfbe019f77454c344ade456f
Romain THIBAUT <romain_thibaut1@ens.univ-artois.fr>
2025-06-12 18:39:11 +0200

    Modification des classes Paiement et PaiementSimple, mais reste sous commentaire car elles causeraient des problèmes pour les test

--- base/src/main/java/fr/univartois/butinfo/sae/odf/model/Paiement.java
+++ base/src/main/java/fr/univartois/butinfo/sae/odf/model/Paiement.java
@@ -1,3 +1,26 @@
+
+// Voici le code compléte mais par manque de temps nous l'avons mis en commentaire car il aurait fallu modifier les test qui ont été réaliser avant
+
+//package fr.univartois.butinfo.sae.odf.model;
+//
+//public class Paiement {
+//    private double montant;
+//    private MethodePaiement methodePaiement;
+//
+//    public Paiement(double montant, MethodePaiement methodePaiement) {
+//        this.montant = montant;
+//        this.methodePaiement = methodePaiement;
+//    }
+//
+//    public double getMontant() {
+//        return montant;
+//    }
+//
+//    public MethodePaiement getMethodePaiement() {
+//        return methodePaiement;
+//    }
+//}
+
 package fr.univartois.butinfo.sae.odf.model;
 
 public class Paiement {
--- base/src/main/java/fr/univartois/butinfo/sae/odf/model/PaiementSimple.java
+++ base/src/main/java/fr/univartois/butinfo/sae/odf/model/PaiementSimple.java
@@ -1,3 +1,25 @@
+
+// Voici le code compléte mais par manque de temps nous l'avons mis en commentaire car il aurait fallu modifier les test qui ont été réaliser avant
+
+//package fr.univartois.butinfo.sae.odf.model;
+//
+//public class PaiementSimple extends Paiement implements TraitementPaiement{
+//	
+//	public PaiementSimple(double montant, MethodePaiement methodePaiement) {
+//        super(montant, methodePaiement);
+//    }
+//
+//    @Override
+//    public void executePaiement(Paiement p) {
+//        p.getMethodePaiement().payer(p.getMontant());
+//    }
+//
+//    @Override
+//    public void remboursement(Paiement p) {
+//        p.getMethodePaiement().rembourser(p.getMontant());
+//    }
+//}
+
 package fr.univartois.butinfo.sae.odf.model;
 
 public class PaiementSimple extends Paiement implements TraitementPaiement{
